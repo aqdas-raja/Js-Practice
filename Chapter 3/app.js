f3188d3487b99a5d4be7c4a93e84dd3d8855b786
aqdas-raja <aqdasraja66@gmail.com>
2024-02-21 03:28:10 +0500

    UPDATE: Complete chapter 3

---
+++ Chapter 3/app.js
@@ -0,0 +1,23 @@
+// Q1
+
+// var age = 21;
+// alert("I am " + age + " years old" );
+
+// Q2
+
+// var visit = 14;
+// alert("You have visited this site " + visit +  " times")
+
+
+// Q3
+
+// var birthyear = 2001;
+// document.write("My birth year is "  + birthyear, "<br> Datatype of my declared variable is" + typeof birthyear);
+
+// Q4
+
+// var visitorname = "<strong> john doe </strong>", producttitle = "XYZ clothing store", quantity = 5;
+// document.write(visitorname + "ordered",  " " + quantity + " <b>T-Shirt</b>(s) on", " " +producttitle)
+
+
+
