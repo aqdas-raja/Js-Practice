1c3d771a33d7878e24fc305f976c529317269850
aqdas-raja <aqdasraja66@gmail.com>
2024-02-20 22:59:37 +0500

    UPDATE: COMPLETE CHAPTER 1 and 2

---
+++ Chapter 1/app.js
@@ -0,0 +1,22 @@
+// Chapter no 1
+
+// Q1
+
+// alert("Wellcome To Javascript")
+
+// Q2
+
+// alert("Error! please enter a valid password")
+
+// Q3
+
+// alert("Welcome to JS Land \n Happy Coding")
+
+// Q4
+
+// alert("Welcome to Js Land ")
+
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+// alert("Hello... i can run JS through my web browser's console.")
+
---
+++ Chapter 2/app.js
@@ -0,0 +1,52 @@
+// Chapter 2
+
+// Q1
+
+// var username = "";
+
+// Q2
+
+// var myName = "Muhammad Aqdas";
+
+// alert(myName);
+
+// Q3
+
+// var message = "Hello World";
+
+// alert(message);
+
+// Q4
+
+// var fname = "john doe";
+// var age = " 15 years old";
+// var certification = " Certified Mobile Application Development";
+
+// alert(fname);
+// alert(age);
+// alert(certification);
+
+// Q5
+
+// var dish ="PIZZA PIZZ  PIZ PI P";
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// Q6
+
+// var email = "example@gmail.com";
+// alert("my email address is: " + email);
+
+// Q7
+
+// var book = " A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book" + book);
+
+// Q8
+
+// var text = "Yah! i can write HTML content through Javascript";
+// document.write(text);
+
+// Q9
+
+// var symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(symbols);
