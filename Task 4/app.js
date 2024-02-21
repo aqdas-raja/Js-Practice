69dcc1f4cebb43115a0263586a8dd1fa9fe304ec
aqdas-raja <aqdasraja66@gmail.com>
2024-02-22 00:14:16 +0500

    Update: Complete Task 4

---
+++ Task 4/app.js
@@ -0,0 +1,85 @@
+// Chapter 1 (Alerts).
+
+// Q1
+
+// var firstname = "Muhammad";
+// var lastname = "Aqdas";
+// var email = "example@gmail.com";
+// var phonenumber = 3334445556;
+// var password = "javascript321";
+
+// alert("My first name is " + firstname);
+// alert("My last name is " + lastname);
+// alert("This is my email " + email);
+// alert("My Ph no is " + phonenumber);
+// alert("This is my password " + password);
+
+
+// Q2
+
+// var study = "I'm learning JavaScript";
+// alert(study);
+
+// Q3
+
+// var like = "I Love JavaScript ♥️";
+// alert(like);
+
+// Chapter 2 (Variables for string).
+
+// Q1
+
+// var firstName = "John"
+// alert(firstName);
+
+// Q2
+
+// var myVariable;
+// myVariable = "This is a string assigned to myVariable";
+
+// Q3
+
+// var teamName = "Everlush";
+// alert(teamName);
+
+// Q4
+
+// var bestMan = "Charlie";
+// bestMan = "John";
+
+// Chapter 3 (Variable for Numbers)
+
+// Q1
+
+// var caseQty;
+
+// Q2
+
+// var caseQty;
+// caseQty = 144;
+
+// Q3
+
+// var num = 9;
+
+// Q4
+
+// var a;
+// a = 2 + 2;
+
+// Q5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var ordertotal = merchTotal + shippingCharge;
+
+// alert(ordertotal);
+
+// Q6
+
+// var number = 5;
+// number = number + 3;
+
+
+
+
