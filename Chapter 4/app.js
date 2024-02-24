b72fe9fd3a3129cca3f7a8d596ede45e837b105f
aqdas-raja <aqdasraja66@gmail.com>
2024-02-24 21:06:59 +0500

    Update: Complete Chapter 4

---
+++ Chapter 4/app.js
@@ -0,0 +1,48 @@
+// Chapter 4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+
+// Q1
+
+// var myName = "John" , gender = "Male" , age = 21;
+
+// Q2
+
+// Five legal variable
+
+// var $myName;
+// var NAME;
+// var _age;
+// var gender;
+// var myVariable;
+
+// Five illegal variable
+
+// var 4name; (Variable names cannot start with a number)
+// my-variable; (Variable names cannot contain hyphens)
+// var; (Keywords such as var cannot be used as variable names)
+// first name (Variable names cannot contain spaces)
+// myVariable! (Variable names cannot contain special characters other than _ and $)
+
+// Q3
+
+// var heading = "<h1> Rules for naming JS variables </h1>";
+// var varName =
+//   "<b> numbers , $ </b> and <b>_underscore </b>. for example: <b>$my_1stVariable</b>.";
+// var ways =
+//   "<b>$</b> or <b>_</b> for example <b>$name</b> or <b>_name</b> or <b>name</b>.";
+// var rule = "<b>Sensitive</b>.";
+// var rule2 = "<b>Keywords</b>.";
+
+// document.write(
+//   heading +
+//     "Variable names can only contain: " +
+//     varName +
+//     "<br>" +
+//     "Variables must begin with a: " +
+//     ways +
+//     "<br>" +
+//     "Variable names are case " +
+//     rule +
+//     "<br>" +
+//     "Variable names should not be JS " +
+//     rule2
+// );
