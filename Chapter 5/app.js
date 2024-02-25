4578813932254d611add15d53e0a4988a2e64357
aqdas-raja <aqdasraja66@gmail.com>
2024-02-26 01:51:16 +0500

    Update: Chapter 5

---
+++ Chapter 5/app.js
@@ -0,0 +1,110 @@
+// Chapter 5
+
+// Q1
+
+// var firstNumber = 3;
+// var secondNumber = 5;
+// var sum = firstNumber + secondNumber;
+
+// document.write("Sum of " + firstNumber +  " and " + secondNumber + " is " + sum );
+
+// Q2
+
+// var firstNumber = 5;
+// var secondNumber = 2;
+// var sub = firstNumber - secondNumber;
+
+// document.write("Subtraction of " + firstNumber + " and " + secondNumber + " is " + sub);
+
+// var firstNumber = 6;
+// var secondNumber = 4;
+// var division = firstNumber / secondNumber;
+
+// document.write("Division of " + firstNumber + " and " + secondNumber + " is " + division)
+
+// var firstNumber = 3;
+// var secondNumber = 4;
+// var multiply = firstNumber * secondNumber;
+
+// document.write("Multiplication of " + firstNumber + " and " + secondNumber + " is " + multiply)
+
+// Q3
+// var a; // a. Declare a variable.
+// document.write("Value after variable declaration is: " + a + "<br>"); // b. Show the value of the variable after declaration
+
+// a = 5; // c. Initialize the variable with some number.
+// document.write("Initial value: " + a + "<br>"); // d. Show the initial value of the variable
+
+// a++; // e. Increment the variable.
+// document.write("Value after increment is: " + a + "<br>"); // f. Show the value of the variable after increment
+
+// a += 7; // g. Add 7 to the variable.
+// document.write("Value after addition is: " + a + "<br>"); // h. Show the value of the variable after addition
+
+// a--; // i. Decrement the variable.
+// document.write("Value after decrement is: " + a + "<br>"); // j. Show the value of the variable after decrement
+
+// var remainder = a % 3; // k. Calculate the remainder after dividing the variable's value by 3
+// document.write("The remainder is: " + remainder); // l. Output the remainder
+
+// Q4
+
+// var ticket = 600;
+// ticket *= 5;
+
+// document.write("Total cost of 5 tickets to a movie is " + ticket + " PKR ")
+
+// Q5
+
+// var table = 3;
+
+// document.write(
+//   "<h1>Table of " +
+//     3 +
+//     ".</h1>" +
+//     "<br>" +
+//     " 3 x 1 = " +
+//     table * 1 +
+//     "<br>" +
+//     " 3 x 2 = " +
+//     table * 2 +
+//     "<br>" +
+//     " 3 x 3 = " +
+//     table * 3 +
+//     "<br>" +
+//     " 3 x 4 = " +
+//     table * 4 +
+//     "<br>" +
+//     " 3 x 5 = " +
+//     table * 5 +
+//     "<br>" +
+//     " 3 x 6 = " +
+//     table * 6 +
+//     "<br>" +
+//     " 3 x 7 = " +
+//     table * 7 +
+//     "<br>" +
+//     " 3 x 8 = " +
+//     table * 8 +
+//     "<br>" +
+//     " 3 x 9 = " +
+//     table * 9 +
+//     "<br>" +
+//     " 3 x 10 = " +
+//     table * 10
+// );
+
+// Q6
+
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F" + "<br>")
+
+// var fahrenheitTemperature2 = 70;
+
+// var celsiusTemperature2 = (fahrenheitTemperature2 * 5/9) -32;
+
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C" )
+
+
