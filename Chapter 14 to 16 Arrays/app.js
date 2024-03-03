42d1aeacffc02ffedc0a4881b25f92d5015eae29
aqdas-raja <aqdasraja66@gmail.com>
2024-03-04 01:21:53 +0500

    Update: Chapter Arrays

---
+++ Chapter 14 to 16 Arrays/app.js
@@ -0,0 +1,78 @@
+// Arrays In Javascript
+
+// Q1
+
+// var studentNames = [];
+
+// Q2
+
+// var arrObj = [];
+
+// Q3
+
+// var studentNames = ["Ali", "Hamza", "Rehman"];
+
+// Q4
+
+// var studentAge = [15, 23, 30];
+
+// Q5
+
+// var myBoolean = true;
+
+// Q6
+
+// var mixArr = ["Ali", 15, true];
+
+// Q6
+
+// var qualification = [
+//   "1) SSC",
+//   "2) HSC",
+//   "3) BCS",
+//   "4) BS",
+//   "5) BCOM",
+//   "6) MS",
+//   "7) M.",
+//   "8) M.Phil",
+//   "9) PhD"
+// ];
+
+// document.write(
+//   "Qualifications:" +
+//     "<br>" +
+//     "<br>" +
+//     qualification[0] +
+//     "<br>" +
+//     qualification[1] +
+//     "<br>" +
+//     qualification[2] +
+//     "<br>" +
+//     qualification[3] +
+//     "<br>" +
+//     qualification[4] +
+//     "<br>" +
+//     qualification[5] +
+//     "<br>" +
+//     qualification[6] +
+//     "<br>" +
+//     qualification[7] +
+//     "<br>" +
+//     qualification[8]
+// );
+
+// Q7
+
+var studentNames = ["Ali", "Hamza", "Raju"];
+
+var studentScore = [320, 230, 480];
+
+var percentage = []
+for(i= 0; i < studentScore.length; i++){
+    percentage[i] = (studentScore[i]/ 500) * 100;
+}
+
+
+
+
+
